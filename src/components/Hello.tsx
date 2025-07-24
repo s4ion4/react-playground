f2d50df9192197f43d1e84418d64c24109c45fd6
reitenshi <goldenbough8891@outlook.jp>
2025-07-25 00:24:40 +0900

    feat: Contextの検証

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,16 +1,26 @@
+import React from "react";
+
 type HelloProps = {
   text: string;
   onClick: () => void;
 };
 
+const signInContext = React.createContext("");
+
 const Hello = (props: HelloProps) => {
   const { text, onClick } = props;
 
   return (
-    <div>
-      <h1>{text}</h1>
-      <button onClick={onClick}>{text}</button>
-    </div>
+    <signInContext.Consumer>
+      {(contextValue) => {
+        return (
+          <div>
+            <h1>{text}</h1>
+            <button onClick={onClick}>{contextValue}</button>
+          </div>
+        );
+      }}
+    </signInContext.Consumer>
   );
 };
 
@@ -18,8 +28,11 @@ const Greeting = () => {
   const onClick = () => {
     console.log("Button clicked!");
   };
-
-  return <Hello text="Hello world!" onClick={onClick} />;
+  return (
+    <signInContext.Provider value="Sign In">
+      <Hello text="Hello world!" onClick={onClick} />
+    </signInContext.Provider>
+  );
 };
 
 export default Greeting;
